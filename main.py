46bcfae91728efdf4a6219107d428380c0147999
MarkerViktor <52465523+MarkerViktor@users.noreply.github.com>
2019-07-05 00:52:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
     hours = time1[0]
     minutes = time1[1]
     seconds = time1[2]
-    if int(hours) == 0 and int(minutes) == 52 and int(seconds) == 0:
+    if int(hours) == 0 and int(minutes) == 54 and int(seconds) == 0:
         response = requests.get("http://wttr.in/Томск", params={'format':3, 'M': ''})
         viktor.massege_send(vk,message = 'Погода в Томске:\n' + response.text.strip())
     #misha.massege_send(vk, message = str(random.randint(0, 1000000000)))
