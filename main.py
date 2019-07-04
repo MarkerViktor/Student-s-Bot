f8cbe4ca0a24732ada52b4b94c47d735ca0043f3
MarkerViktor <52465523+MarkerViktor@users.noreply.github.com>
2019-07-05 00:18:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while True:
     time = time.split(' ')[4].split(':')
     hours = time[0]
     minutes = time[1]
-    response = requests.get(f'http://wttr.in/{city}', params={'format': 2, 'M': ''})
+    response = requests.get("http://wttr.in/'Томск'", params={'format': 2, 'M': ''})
     viktor.massege_send(vk,message = response.text.strip())
     time.sleep(1850)
     #misha.massege_send(vk, message = str(random.randint(0, 1000000000)))
