2f9474a5f7766a3f0c6880dfc28faad1758863b8
MarkerViktor <52465523+MarkerViktor@users.noreply.github.com>
2019-07-04 23:58:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ viktor = cl.User(94138203, name = 'Виктор', surname= 'Маркер', group
 misha = cl.User(175750670)
 while True:
     time = time.ctime(time.time()+25200)
-    hours = time[10:11]
-    minuts = time[13:14]
-    viktor.massege_send(vk,message = hours + ' ' +  minuts)
+    hours = time[10:12]
+    minuts = time[13:15]
+    viktor.massege_send(vk,message = (hours + ' ' +  minuts))
     time.sleep(1850)
     #misha.massege_send(vk, message = str(random.randint(0, 1000000000)))
 
