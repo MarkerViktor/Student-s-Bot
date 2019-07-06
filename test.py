3393237633c05e8358faef167a88160f4eb1e07f
MarkerViktor <52465523+MarkerViktor@users.noreply.github.com>
2019-07-06 21:41:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(config.DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
 cursor.execute('SELECT name, surname FROM users WHERE id=94138203')
 print(cursor.fetchall())
