6992d37be12b3a7ecb5b867da492e1fbac88599a
MarkerViktor <52465523+MarkerViktor@users.noreply.github.com>
2019-07-06 21:21:51 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+import psycopg2
+DATABASE_URL = os.environ['DATABASE_URL']
+def connect();
+  conn = psycopg2.connect(config.DATABASE_URL, sslmode='require')
+  return conn.cursor()
+def search():
+  cursor = connect()
+  cursor.execute(SELECT name, surname FROM users WHERE id=94138203)
+  print(cursor.fetchall())
