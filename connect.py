e8d934eb93c6b766e8940e82703ce4217fd7de9f
Viktor <MarkerVIktor@outlook.com>
2019-07-07 04:24:36 +0700

    Обновление

--- connect.py
+++ connect.py
@@ -10,13 +10,13 @@ def start():
     longpoll = vk_api.bot_longpoll.VkBotLongPoll(vk_session, config.group_id)
     try:
         conn = psycopg2.connect(config.DATABASE_host, sslmode='require')
-        print('CONNECTED')
+        print('Base is connected')
     except psycopg2.ProgrammingError:
         conn = psycopg2.connect(dbname=config.DATABASE_name,
                                 user=config.DATABASE_user,
                                 password=config.DATABASE_password,
                                 host=config.DATABASE_host)
-        print('CONNECTED')
+        print('Base is connected')
     else:
         print("Can't become connected")
     vk = vk_session.get_api()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import connect
 
+connect.start()
 
 
 
